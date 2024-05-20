2c80adb18a3de80234723cf670789a07303b4c38
Chad Eldridge <chad.eldridge@gmail.com>
2024-05-20 17:39:55 -0400

    Heavy refactoring and testing. Now in a good base state.

---
+++ connections/servers.go
@@ -0,0 +1,148 @@
+package connections
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net"
+	"strconv"
+
+	validator "github.com/go-playground/validator/v10"
+)
+
+var validate = validator.New()
+
+type Server struct {
+	name     string
+	hostname string
+	ip       net.IP
+	port     string
+	useIP    bool
+	Connection
+	Results *bytes.Buffer
+	Logs    *bytes.Buffer
+}
+
+// NewServer creates a new Server with a display Name and connection Protocol to use. If port is
+// set to 0, the default port for the protocol will be used.
+func NewServer(hostname string, port int, results, logs *bytes.Buffer) (Server, error) {
+	s := Server{}
+	if err := s.SetHostname(hostname); err != nil {
+		return s, err
+	}
+
+	if err := s.SetPort(port); err != nil {
+		return s, err
+	}
+
+	s.Results = results
+	s.Logs = logs
+
+	return s, nil
+}
+
+func NewPlacholderServer() Server { return Server{name: "Empty Server Profile"} }
+
+func (s Server) Name() string     { return s.name }
+func (s Server) Hostname() string { return s.hostname }
+func (s Server) IP() string       { return s.ip.String() }
+func (s Server) Port() string     { return s.port }
+func (s Server) UseIP() bool      { return s.useIP }
+func (s Server) IsEmpty() bool    { return s.hostname == "" }
+
+func (s Server) TestConnection() error                  { return s.Connection.TestConnection(s) }
+func (s Server) Run(cmd, expect string) (string, error) { return s.Connection.Run(s, cmd, expect) }
+
+// GetAddr determines the address to connect to. Returns "hostname:port" or "ip:port". If port is
+// set to 0, GetAddr uses protocol's default port instead.
+func (s Server) GetAddr() string {
+	host := s.hostname
+	if s.useIP {
+		host = s.IP()
+	}
+
+	return net.JoinHostPort(host, s.port)
+}
+
+// SetUseIP sets the useIP field to true or false. This field is used to determine if the ip field
+// should be used instead of the hostname.
+func (s *Server) SetUseIP(flag bool) { s.useIP = flag }
+
+// SetName sets the display name for Server.
+func (s *Server) SetName(name string) error {
+	// Add verification to prevent escape character and other exploits.
+	s.name = name
+	return nil
+}
+
+// SetHostname sets the hostname to use for the server. If the hostname is an IP
+// it will set both the Hostname and IP to the IP.
+func (s *Server) SetHostname(hostname string) error {
+	if hostname == "" {
+		return fmt.Errorf("expected valid hostname, got empty string")
+	}
+
+	if err := s.SetIP(hostname); err == nil {
+		s.hostname = hostname
+		return nil
+	}
+
+	if err := validate.Var(hostname, "hostname"); err != nil {
+		return err
+	}
+
+	s.hostname = hostname
+	if s.name == "" {
+		s.name = hostname
+	}
+
+	return nil
+}
+
+// SetIP sets the ip address to be used for connecting to the server. If Server.hostname is set to
+// an ip, this field will automatically be set. Setting this field prevents hostname lookup.
+// If hostname is unset, hostname will be set to ip.
+func (s *Server) SetIP(ip string) error {
+	if ip == "" {
+		return fmt.Errorf("expected valid IPv4 string, got empty string")
+	}
+
+	if i := net.ParseIP(ip); i != nil {
+		s.ip = i
+		s.useIP = true
+		if s.hostname == "" {
+			s.hostname = i.String()
+		}
+		return nil
+	}
+
+	return fmt.Errorf("ip not valid: %s", ip)
+}
+
+// SetPort sets the Port to be used when connecting to the server. Empty uses Protocol default.
+func (s *Server) SetPort(port int) error {
+	if port < 0 || port > 65535 {
+		return fmt.Errorf("port must be between 0 and 65535")
+	}
+
+	s.port = strconv.Itoa(port)
+	return nil
+}
+
+// SetHandler sets the Handler interface to be used for connecting to the server.
+func (s *Server) SetHandler(handler Connection) error {
+	if handler == nil {
+		return errors.New("could not set handler, provided handler was nil")
+	}
+	if handler.IsEmpty() {
+		return errors.New("could not set handler, provided handler was empty")
+	}
+
+	s.Connection = handler
+
+	if s.port == "0" {
+		s.port = strconv.Itoa(handler.DefaultPort())
+	}
+
+	return nil
+}
---
+++ connections/servers_test.go
@@ -0,0 +1,154 @@
+package connections
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type serverInputs struct {
+	Name     string
+	Hostname string
+	IP       string
+	Protocol string
+	Port     int
+}
+
+type serverWants struct {
+	Name     string
+	Hostname string
+	IP       string
+	Protocol
+	Port string
+}
+
+var (
+	results bytes.Buffer
+	logs    bytes.Buffer
+
+	goodInputs = serverInputs{
+		Name:     "test.home Test Server",
+		Hostname: "test.home",
+		IP:       "192.168.50.105",
+		Protocol: "ssh",
+		Port:     22,
+	}
+
+	badInputs = serverInputs{
+		Name:     "test.home Test Server",
+		Hostname: "89ey*(#@F*)89023r",
+		IP:       "192.168.501.105",
+		Protocol: "blah",
+		Port:     -1,
+	}
+
+	goodWant = serverWants{
+		Name:     goodInputs.Name,
+		Hostname: goodInputs.Hostname,
+		IP:       "192.168.50.105",
+		Protocol: SSH,
+		Port:     "22",
+	}
+
+	badWant = serverWants{
+		Name:     "", // Change this when exploit validation is added for Name
+		Hostname: "",
+		IP:       "<nil>",
+		Protocol: INVALID,
+		Port:     "",
+	}
+)
+
+func testNewServer(t *testing.T, input serverInputs) Server {
+	got, err := NewServer(input.Hostname, input.Port, &results, &logs)
+	got.SetName(input.Name)
+	require.Nil(t, err, "error recieved for NewServer", err)
+	require.Equal(t, goodWant.Hostname, got.Hostname())
+	require.Equal(t, goodWant.Name, got.Name())
+	require.Equal(t, goodWant.Port, got.Port())
+	return got
+}
+
+func TestServersNewServer(t *testing.T) {
+	// Good Protol
+	testNewServer(t, goodInputs)
+
+	// Bad Protocol
+	got, err := NewServer(badInputs.Hostname, badInputs.Port, &results, &logs)
+	require.NotNil(t, err, "did not recieve error using NewServer with bad Protocol", err, badInputs.Protocol)
+	require.Equal(t, badWant.Hostname, got.Hostname())
+	require.Equal(t, badWant.Name, got.Name())
+	require.Equal(t, badWant.Port, got.Port())
+}
+
+func TestServersSetHostname(t *testing.T) {
+	// Good Hostname
+	got := testNewServer(t, goodInputs)
+	err := got.SetHostname(goodInputs.Hostname)
+	require.Nil(t, err, "error recieved when setting a good hostname", err, goodInputs.Hostname)
+	require.Equal(t, goodWant.Hostname, got.Hostname(), "hostname did not match")
+	// IP should be empty because we should not be resolving a non-IP hostname here.
+	require.Equal(t, badWant.IP, got.IP(), "got.IP() was not <nil> when setting a good hostname")
+
+	// Good IP Hostname
+	got, _ = NewServer(goodInputs.Name, goodInputs.Port, &results, &logs)
+	err = got.SetHostname(goodInputs.IP)
+	require.Nil(t, err, "error recieved when setting a good IP hostname", err, goodInputs.IP)
+	require.Equal(t, goodInputs.IP, got.Hostname(), "ip hostname did not match")
+	require.Equal(t, goodWant.IP, got.IP(), "got.IP() did not match expected ip")
+
+	// Empty Hostname
+	got, _ = NewServer(goodInputs.Name, goodInputs.Port, &results, &logs)
+	err = got.SetHostname("")
+	require.NotNil(t, err, "did not recieve error when setting an empty hostname")
+	require.Equal(t, badWant.Hostname, got.Hostname(), "hostname was not empty when setting an empty hostname")
+	require.Equal(t, badWant.IP, got.IP(), "got.IP() was not <nil> when setting an empty hostname")
+
+	// Bad Hostname
+	got, _ = NewServer(goodInputs.Name, goodInputs.Port, &results, &logs)
+	err = got.SetHostname(badInputs.Hostname)
+	require.NotNil(t, err, "did not recieve error when setting a bad hostname", badInputs.Hostname)
+	require.Equal(t, badWant.IP, got.IP(), "got.IP() was not <nil> when setting a bad hostname")
+
+	// Bad IP Hostname
+	got, _ = NewServer(goodInputs.Name, goodInputs.Port, &results, &logs)
+	err = got.SetHostname(badInputs.IP)
+	require.NotNil(t, err, "did not recieve error when setting a bad IP hostname", badInputs.IP)
+	require.Equal(t, badWant.Hostname, got.Hostname(), "hostname not set for bad IP")
+	require.Equal(t, badWant.IP, got.IP(), "got.IP() was not <nil> when setting a bad IP hostname")
+}
+
+func TestServersSetIP(t *testing.T) {
+	// Good IP
+	got := testNewServer(t, goodInputs)
+	err := got.SetIP(goodInputs.IP)
+	require.Nil(t, err, "error recieved when setting good IP", goodInputs.IP)
+	require.Equal(t, goodWant.IP, got.IP(), "got.IP() did not match expected ip")
+
+	// Empty IP
+	got, _ = NewServer(goodInputs.Name, goodInputs.Port, &results, &logs)
+	err = got.SetIP("")
+	require.NotNil(t, err, "did not recieve error when setting an empty IP")
+	require.Equal(t, badWant.IP, got.IP(), "got.IP() was not <nil> when setting a empty IP")
+
+	// Bad IP
+	got, _ = NewServer(goodInputs.Name, goodInputs.Port, &results, &logs)
+	err = got.SetIP(badInputs.IP)
+	require.NotNil(t, err, "did not recieve error when setting a bad IP", badInputs.IP)
+	require.Equal(t, badWant.IP, got.IP(), "got.IP() was not <nil> when setting a bad IP")
+}
+
+func TestServersSetPort(t *testing.T) {
+	// Good Port
+	got := testNewServer(t, goodInputs)
+	err := got.SetPort(goodInputs.Port)
+	require.Nil(t, err, "error recieved when setting good Port", goodInputs.Port)
+	require.Equal(t, goodWant.Port, got.Port(), "got.Port() did not match expected port")
+
+	// Bad Port
+	got, _ = NewServer(goodInputs.Name, goodInputs.Port, &results, &logs)
+	err = got.SetPort(badInputs.Port)
+	require.NotNil(t, err, "did not recieve error when setting a bad Port", badInputs.Port)
+	require.Equal(t, badWant.Port, got.Port(), "got.Port() was not 0 when setting a bad port", got.Port())
+}
