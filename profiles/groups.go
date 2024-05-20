d415035d6ba4f6ae6555bfca01ba6a1cecb63ff0
Chad Eldridge <chad.eldridge@gmail.com>
2024-05-20 17:40:22 -0400

    Minor changes. Safety save.

--- profiles/groups.go
+++ profiles/groups.go
@@ -1,10 +1,14 @@
 package profiles
 
+import "github.com/chadeldridge/cuttle/connections"
+
 type Group struct {
 	Name    string
-	Servers []Server
+	Servers []connections.Server
 }
 
+/*
 func NewGroup(name string, servers ...Server) Group {
 	return Group{Name: name, Servers: servers}
 }
+*/
