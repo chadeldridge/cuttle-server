0af3f5c2d3b43e900382230a554a80c1bbe79260
Chad Eldridge <chad.eldridge@gmail.com>
2024-05-20 17:35:39 -0400

    Moved to helpers. Might not be needed at all. Will remove later if so.

--- connections/params.go
+++
@@ -1,5 +0,0 @@
-package connections
-
-type Param map[string]interface{}
-
-// func NewParam(key string, value interface{})
---
+++ helpers/params.go
@@ -0,0 +1,5 @@
+package helpers
+
+type Param map[string]any
+
+// func NewParam(key string, value interface{})
