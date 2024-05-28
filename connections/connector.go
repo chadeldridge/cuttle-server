8ea833e8438011db8918167148fc740b7dd4332e
Chad Eldridge <chad.eldridge@gmail.com>
2024-05-28 13:01:08 -0400

    Renamed connection to connector.

---
+++ connections/connector.go
@@ -0,0 +1,14 @@
+package connections
+
+type Connector interface {
+	Protocol() Protocol
+	User() string
+	DefaultPort() int
+	IsEmpty() bool
+	IsValid() bool
+	TestConnection(server Server) error
+	Run(server Server, cmd string, expect string) error
+	Log(server Server, txt string)
+	Open(server Server) error
+	Close()
+}
