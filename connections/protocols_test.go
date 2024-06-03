0e4862f53cf6b40053ec1a91352c06ea5b68b01b
Chad Eldridge <chad.eldridge@gmail.com>
2024-06-03 11:58:27 -0400

    Moved to using subtests.

--- connections/protocols_test.go
+++ connections/protocols_test.go
@@ -1,14 +1,22 @@
 package connections
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func testStringToProtocol(t *testing.T, in string, want Protocol) {
-	got := StringToProtocol(in)
-	require.Equal(t, want, got, "connections.testStringToProtocol: returned Protocol did not match")
+func testStringToProtocol(t *testing.T, proto string, want Protocol) {
+	got := StringToProtocol(proto)
+	testName := proto
+	if proto == "" {
+		testName = "empty"
+	}
+
+	t.Run(fmt.Sprintf("%s protocol", testName), func(t *testing.T) {
+		require.Equal(t, want, got, "connections.testStringToProtocol: returned Protocol did not match")
+	})
 }
 
 func TestProtocolsStringToProtocol(t *testing.T) {
@@ -16,5 +24,5 @@ func TestProtocolsStringToProtocol(t *testing.T) {
 	testStringToProtocol(t, "Rdp", RDP)
 	testStringToProtocol(t, "TELNET", TELNET)
 	testStringToProtocol(t, "", INVALID)
-	testStringToProtocol(t, "testing", INVALID)
+	testStringToProtocol(t, "bad", INVALID)
 }
