f5c6b110b6eed081592a62032c0ccc025722c555
Chad Eldridge <chad.eldridge@gmail.com>
2024-06-05 13:22:43 -0400

    Combined struct and interface implementations to match golang conventions.

--- connections/mockConnector.go
+++ connections/mockConnector.go
@@ -1,6 +1,13 @@
 package connections
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
 
 const (
 	MockDefaultPort = 555
@@ -29,5 +36,90 @@ func (h *MockConnector) SetUser(username string) error {
 	}
 
 	h.user = username
+	return nil
+}
+
+//						//
+//	Connector Interface Implementation	//
+//						//
+
+func (c MockConnector) IsConnected() bool  { return c.isConnected }
+func (c MockConnector) IsActive() bool     { return c.hasSession }
+func (c MockConnector) Protocol() Protocol { return MockProtocol }
+func (c MockConnector) User() string       { return c.user }
+func (c MockConnector) DefaultPort() int   { return MockDefaultPort }
+func (c MockConnector) IsEmpty() bool      { return c.user == "" }
+func (c MockConnector) IsValid() bool      { return c.user != "" }
+
+func (c MockConnector) Validate() error {
+	if c.user == "" {
+		return ErrInvalidEmtpyUser
+	}
+
+	return nil
+}
+
+func (c *MockConnector) Open(server Server) error {
+	if err := c.Validate(); err != nil {
+		return err
+	}
+
+	c.isConnected = true
+	return nil
+}
+
+func (c MockConnector) TestConnection(server Server) error {
+	localhost, err := os.Hostname()
+	if err != nil {
+		return fmt.Errorf("connections.SSHConnector.TestConnection: error retrieving local hostname: %s", err)
+	}
+
+	expect := fmt.Sprintf("cuttle from %s ok", localhost)
+	return c.Run(server, fmt.Sprintf("echo '%s'", expect), expect)
+}
+
+func (c MockConnector) Run(server Server, cmd, exp string) error {
+	if !c.isConnected {
+		return ErrNotConnected
+	}
+
+	cmd = strings.TrimSpace(cmd)
+	if cmd == "" {
+		return ErrEmtpyCmd
+	}
+
+	if exp == "" {
+		return ErrEmtpyExp
+	}
+
+	// We have to split cmd into the command name and args for exec to work. This adds
+	// complication but we do not have a choice.
+	parts := strings.SplitN(cmd, " ", 2)
+	eventTime := time.Now()
+	out, err := exec.Command(parts[0], parts[1]).Output()
+	if err != nil {
+		server.Log(eventTime, err.Error())
+		server.PrintResults(eventTime, "error", err)
+		return err
+	}
+
+	// Log the full output of the command.
+	server.Log(eventTime, string(out))
+
+	ok := foundExpect(out, exp)
+	if !ok {
+		server.PrintResults(eventTime, "failed", nil)
+	}
+
+	server.PrintResults(eventTime, "ok", nil)
+	return nil
+}
+
+func (c *MockConnector) Close(force bool) error {
+	if !c.isConnected {
+		return ErrNotConnected
+	}
+
+	c.isConnected = false
 	return nil
 }
--- connections/sshConnector.go
+++ connections/sshConnector.go
@@ -1,8 +1,11 @@
 package connections
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"log"
+	"os"
 	"regexp"
 	"time"
 
@@ -121,4 +124,132 @@ func foundExpect(data []byte, expect string) bool {
 	return matched
 	// m := bytes.Index(data, []byte(expect))
 	// return m > -1
+}
+
+//						//
+//	Connector Interface Implementation	//
+//						//
+
+func (c *SSHConnector) IsConnected() bool  { return c.isConnected }
+func (c *SSHConnector) IsActive() bool     { return c.hasSession }
+func (c *SSHConnector) Protocol() Protocol { return SSHProtocol }
+func (c *SSHConnector) User() string       { return c.user }
+func (c *SSHConnector) DefaultPort() int   { return SSHDefaultPort }
+func (c *SSHConnector) IsEmpty() bool      { return c.user == "" }
+func (c *SSHConnector) IsValid() bool      { err := c.Validate(); return err == nil }
+
+func (c SSHConnector) Validate() error {
+	if c.user == "" {
+		return ErrInvalidEmtpyUser
+	}
+
+	if len(c.auth) < 1 {
+		return ErrInvalidNoAuthMethod
+	}
+
+	return nil
+}
+
+func (c *SSHConnector) Open(server Server) error {
+	if err := c.Validate(); err != nil {
+		return err
+	}
+
+	config := &ssh.ClientConfig{
+		User:            c.user,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Auth:            c.auth,
+	}
+
+	// log.Print("Dialing server...")
+	client, err := ssh.Dial("tcp", server.GetAddr(), config)
+	if err != nil {
+		server.Log(time.Now(), err.Error())
+		server.PrintResults(time.Now(), "error", err)
+		return err
+	}
+
+	c.isConnected = true
+	c.Client = client
+	// log.Print("done.")
+	// INCOMPLETE: Add a keepalive later.
+	return nil
+}
+
+func (c *SSHConnector) TestConnection(server Server) error {
+	localhost, err := os.Hostname()
+	if err != nil {
+		return fmt.Errorf("connections.SSHConnector.TestConnection: error retrieving local hostname: %s", err)
+	}
+
+	expect := fmt.Sprintf("cuttle from %s ok", localhost)
+	return c.run(server, fmt.Sprintf("echo '%s'", expect), expect)
+}
+
+func (c *SSHConnector) Run(server Server, cmd string, exp string) error {
+	return c.run(server, cmd, exp)
+}
+
+func (c *SSHConnector) run(server Server, cmd string, exp string) error {
+	if cmd == "" {
+		return ErrEmtpyCmd
+	}
+
+	if exp == "" {
+		return ErrEmtpyExp
+	}
+
+	if !c.isConnected {
+		return ErrNotConnected
+	}
+
+	err := c.OpenSession(server)
+	if err != nil {
+		return err
+	}
+
+	// We have to close the session each time or it will block further command execution.
+	defer c.CloseSession()
+
+	// Set ssh.Session.Stdout so we capture the output
+	var b bytes.Buffer
+	c.Session.Stdout = &b
+	eventTime := time.Now()
+
+	// log.Print("   - Running cmd...")
+	err = c.Session.Run(cmd)
+	if err != nil {
+		server.Log(eventTime, err.Error())
+		server.PrintResults(eventTime, "error", err)
+		return err
+	}
+	// log.Print("done.")
+
+	// Log the full output of the command
+	server.Log(eventTime, b.String())
+
+	// Match results to the expected results and print
+	ok := foundExpect(b.Bytes(), exp)
+	if !ok {
+		server.PrintResults(eventTime, "failed", nil)
+		return nil
+	}
+
+	server.PrintResults(eventTime, "ok", nil)
+	return nil
+}
+
+func (c *SSHConnector) Close(force bool) error {
+	if c.hasSession {
+		// If we don't want to foce close the connection return an error.
+		if !force {
+			return ErrSessionActive
+		}
+
+		// Otherwise force the session closed.
+		c.CloseSession()
+	}
+
+	c.isConnected = false
+	return c.Client.Close()
 }
