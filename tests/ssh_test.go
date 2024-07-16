054844e9fab3944b644ec1d3c6bf89ed096889f2
Chad Eldridge <chad.eldridge@gmail.com>
2024-07-16 12:58:46 -0400

    refactor: Added tests for Set functions.

--- tests/ssh_test.go
+++ tests/ssh_test.go
@@ -45,6 +45,89 @@ func TestSSHNewSSHTest(t *testing.T) {
 	})
 }
 
+func TestTilesSetHideCmd(t *testing.T) {
+	require := require.New(t)
+	test := SSHTest{}
+
+	t.Run("default", func(t *testing.T) {
+		require.False(test.HideCmd, "hideCmd returned true")
+	})
+
+	t.Run("true", func(t *testing.T) {
+		test.SetHideCmd(true)
+		require.True(test.HideCmd, "hideCmd returned false")
+	})
+
+	t.Run("false", func(t *testing.T) {
+		test.SetHideCmd(false)
+		require.False(test.HideCmd, "hideCmd returned true")
+	})
+}
+
+func TestTilesSetHideExp(t *testing.T) {
+	require := require.New(t)
+	test := SSHTest{}
+
+	t.Run("default", func(t *testing.T) {
+		require.False(test.HideExp, "hideExp returned true")
+	})
+
+	t.Run("true", func(t *testing.T) {
+		test.SetHideExp(true)
+		require.True(test.HideExp, "hideExp returned false")
+	})
+
+	t.Run("false", func(t *testing.T) {
+		test.SetHideExp(false)
+		require.False(test.HideExp, "hideExp returned true")
+	})
+}
+
+func TestTilesSetCmd(t *testing.T) {
+	require := require.New(t)
+	test := SSHTest{}
+
+	t.Run("valid", func(t *testing.T) {
+		err := test.SetCmd("ping google.com")
+		require.NoError(err, "SetCmd() returned an error: %s", err)
+	})
+
+	// INCOMPLETE: Update after SetCmd checks for valid command.
+	/*
+		t.Run("invalid", func(t *testing.T) {
+			err := tile.SetCmd("invalid command")
+			require.Error(err, "SetCmd() did not return an error")
+		})
+	*/
+
+	t.Run("empty", func(t *testing.T) {
+		err := test.SetCmd("")
+		require.Error(err, "SetCmd() did not return an error")
+	})
+}
+
+func TestTilesSetExp(t *testing.T) {
+	require := require.New(t)
+	test := SSHTest{}
+
+	t.Run("valid", func(t *testing.T) {
+		err := test.SetExp("64 bytes from")
+		require.NoError(err, "SetExp() returned an error: %s", err)
+	})
+
+	/*
+		t.Run("invalid", func(t *testing.T) {
+			err := tile.SetExp("invalid expect string")
+			require.Error(err, "SetExp() did not return an error")
+		})
+	*/
+
+	t.Run("empty", func(t *testing.T) {
+		err := test.SetExp("")
+		require.Error(err, "SetExp() did not return an error")
+	})
+}
+
 func TestSSHTestRun(t *testing.T) {
 	require := require.New(t)
 	server := testServerSetup(t)
