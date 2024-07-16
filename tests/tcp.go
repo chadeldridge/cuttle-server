ffff5d8356b3826a6f7df78aaeda7f64d3d54547
Chad Eldridge <chad.eldridge@gmail.com>
2024-07-16 12:57:38 -0400

    refactor: Renamed PortOpen to PortHalfOpen and created a PortOpen which checks for a successful Close().

--- tests/tcp.go
+++ tests/tcp.go
@@ -20,6 +20,24 @@ type TCPTest struct {
 	timeout  time.Duration
 }
 
+// NewTCPPortHalfOpen creates a new Test for tcp port open with the given parameters.
+// name: The name of the test.
+// mustSucceed: If false, the Tile will continue with the test stack if this test fails.
+//
+// These TestArg will be evaluated:
+// "timeout": (int, int64, time.Duration) int/int64 will be converted into time.Second * int.
+func NewTCPPortHalfOpen(name string, mustSucceed bool, port int, args ...TestArg) Test {
+	return Test{
+		Name:        name,
+		MustSucceed: mustSucceed,
+		Tester: &TCPTest{
+			testType: "port_half_open",
+			port:     strconv.Itoa(port),
+			timeout:  getTCPTimeout(args),
+		},
+	}
+}
+
 // NewTCPPortOpen creates a new Test for tcp port open with the given parameters.
 // name: The name of the test.
 // mustSucceed: If false, the Tile will continue with the test stack if this test fails.
@@ -43,6 +61,8 @@ func getTCPTimeout(args []TestArg) time.Duration { return GetTimeout(args, TCPDe
 // Run evaluates TCPTest.testType and runs the appropriate test, passing along server and args.
 func (t TCPTest) Run(server connections.Server, args ...TestArg) error {
 	switch t.testType {
+	case "port_half_open":
+		return PortHalfOpen(t, server, args...)
 	case "port_open":
 		return PortOpen(t, server, args...)
 	default:
@@ -50,8 +70,8 @@ func (t TCPTest) Run(server connections.Server, args ...TestArg) error {
 	}
 }
 
-// PortOpen performs a simple tcp port open test against the server.
-func PortOpen(t TCPTest, server connections.Server, args ...TestArg) error {
+// PortHalfOpen performs a simple tcp port open test against the server and ignores close errors.
+func PortHalfOpen(t TCPTest, server connections.Server, args ...TestArg) error {
 	conn, err := net.DialTimeout(
 		"tcp",
 		net.JoinHostPort(server.GetHostAddr(), t.port),
@@ -63,4 +83,19 @@ func PortOpen(t TCPTest, server connections.Server, args ...TestArg) error {
 
 	conn.Close()
 	return nil
+}
+
+// PortOpen performs a simple tcp port open test against the server and makes sure it successfully
+// closes the connection.
+func PortOpen(t TCPTest, server connections.Server, args ...TestArg) error {
+	conn, err := net.DialTimeout(
+		"tcp",
+		net.JoinHostPort(server.GetHostAddr(), t.port),
+		t.timeout,
+	)
+	if err != nil {
+		return err
+	}
+
+	return conn.Close()
 }
--- tests/tcp_test.go
+++ tests/tcp_test.go
@@ -7,6 +7,50 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTCPNewTCPPortHalfOpen(t *testing.T) {
+	require := require.New(t)
+
+	t.Run("default timeout", func(t *testing.T) {
+		test := NewTCPPortHalfOpen("TCP Test", true, 22)
+		require.Equal("TCP Test", test.Name, "NewTCPTest() Name is not 'TCP Test'")
+		require.True(test.MustSucceed, "NewTCPTest() MustSucceed is not true")
+		require.Equal(
+			"port_half_open",
+			test.Tester.(*TCPTest).testType,
+			"NewTCPTest() testType is not 'port_half_open'",
+		)
+		require.Equal("22", test.Tester.(*TCPTest).port, "NewTCPTest() port is not '22'")
+		require.Equal(TCPDefaultTimeout, test.Tester.(*TCPTest).timeout, "NewTCPTest() timeout is not 3s")
+	})
+
+	t.Run("int timeout", func(t *testing.T) {
+		test := NewTCPPortHalfOpen("TCP Test", true, 22, TestArg{Key: "timeout", Value: 4})
+		require.Equal("TCP Test", test.Name, "NewTCPTest() Name is not 'TCP Test'")
+		require.True(test.MustSucceed, "NewTCPTest() MustSucceed is not true")
+		require.Equal(
+			"port_half_open",
+			test.Tester.(*TCPTest).testType,
+			"NewTCPTest() testType is not 'port_half_open'",
+		)
+		require.Equal("22", test.Tester.(*TCPTest).port, "NewTCPTest() port is not '22'")
+		require.Equal(time.Second*4, test.Tester.(*TCPTest).timeout, "NewTCPTest() timeout is not 3s")
+	})
+
+	t.Run("time.Duration timeout", func(t *testing.T) {
+		timeout := time.Second * 4
+		test := NewTCPPortHalfOpen("TCP Test", true, 22, TestArg{Key: "timeout", Value: timeout})
+		require.Equal("TCP Test", test.Name, "NewTCPTest() Name is not 'TCP Test'")
+		require.True(test.MustSucceed, "NewTCPTest() MustSucceed is not true")
+		require.Equal(
+			"port_half_open",
+			test.Tester.(*TCPTest).testType,
+			"NewTCPTest() testType is not 'port_half_open'",
+		)
+		require.Equal("22", test.Tester.(*TCPTest).port, "NewTCPTest() port is not '22'")
+		require.Equal(timeout, test.Tester.(*TCPTest).timeout, "NewTCPTest() timeout is not 3s")
+	})
+}
+
 func TestTCPNewTCPPortOpen(t *testing.T) {
 	require := require.New(t)
 
@@ -14,7 +58,11 @@ func TestTCPNewTCPPortOpen(t *testing.T) {
 		test := NewTCPPortOpen("TCP Test", true, 22)
 		require.Equal("TCP Test", test.Name, "NewTCPTest() Name is not 'TCP Test'")
 		require.True(test.MustSucceed, "NewTCPTest() MustSucceed is not true")
-		require.Equal("port_open", test.Tester.(*TCPTest).testType, "NewTCPTest() testType is not 'port_open'")
+		require.Equal(
+			"port_open",
+			test.Tester.(*TCPTest).testType,
+			"NewTCPTest() testType is not 'port_open'",
+		)
 		require.Equal("22", test.Tester.(*TCPTest).port, "NewTCPTest() port is not '22'")
 		require.Equal(TCPDefaultTimeout, test.Tester.(*TCPTest).timeout, "NewTCPTest() timeout is not 3s")
 	})
@@ -23,7 +71,11 @@ func TestTCPNewTCPPortOpen(t *testing.T) {
 		test := NewTCPPortOpen("TCP Test", true, 22, TestArg{Key: "timeout", Value: 4})
 		require.Equal("TCP Test", test.Name, "NewTCPTest() Name is not 'TCP Test'")
 		require.True(test.MustSucceed, "NewTCPTest() MustSucceed is not true")
-		require.Equal("port_open", test.Tester.(*TCPTest).testType, "NewTCPTest() testType is not 'port_open'")
+		require.Equal(
+			"port_open",
+			test.Tester.(*TCPTest).testType,
+			"NewTCPTest() testType is not 'port_open'",
+		)
 		require.Equal("22", test.Tester.(*TCPTest).port, "NewTCPTest() port is not '22'")
 		require.Equal(time.Second*4, test.Tester.(*TCPTest).timeout, "NewTCPTest() timeout is not 3s")
 	})
@@ -33,7 +85,11 @@ func TestTCPNewTCPPortOpen(t *testing.T) {
 		test := NewTCPPortOpen("TCP Test", true, 22, TestArg{Key: "timeout", Value: timeout})
 		require.Equal("TCP Test", test.Name, "NewTCPTest() Name is not 'TCP Test'")
 		require.True(test.MustSucceed, "NewTCPTest() MustSucceed is not true")
-		require.Equal("port_open", test.Tester.(*TCPTest).testType, "NewTCPTest() testType is not 'port_open'")
+		require.Equal(
+			"port_open",
+			test.Tester.(*TCPTest).testType,
+			"NewTCPTest() testType is not 'port_open'",
+		)
 		require.Equal("22", test.Tester.(*TCPTest).port, "NewTCPTest() port is not '22'")
 		require.Equal(timeout, test.Tester.(*TCPTest).timeout, "NewTCPTest() timeout is not 3s")
 	})
@@ -42,6 +98,16 @@ func TestTCPNewTCPPortOpen(t *testing.T) {
 func TestTCPRun(t *testing.T) {
 	require := require.New(t)
 	server := testServerSetup(t)
+
+	t.Run("port_half_open", func(t *testing.T) {
+		test := TCPTest{
+			testType: "port_half_open",
+			port:     "22",
+			timeout:  TCPDefaultTimeout,
+		}
+
+		require.NoError(test.Run(server), "TCPTest.Run() returned an error")
+	})
 
 	t.Run("port_open", func(t *testing.T) {
 		test := TCPTest{
@@ -64,6 +130,40 @@ func TestTCPRun(t *testing.T) {
 	})
 }
 
+func TestTCPPortHalfOpen(t *testing.T) {
+	require := require.New(t)
+	server := testServerSetup(t)
+
+	test := TCPTest{
+		testType: "port_half_open",
+		port:     "22",
+		timeout:  TCPDefaultTimeout,
+	}
+
+	// TODO: Need better tests to verify proper behaviour for PortHalfOpen.
+	t.Run("success", func(t *testing.T) {
+		require.NoError(PortHalfOpen(test, server), "PortHalfOpen() returned an error")
+	})
+
+	t.Run("with timeout", func(t *testing.T) {
+		test.timeout = time.Second * 3
+		err := PortHalfOpen(test, server)
+		require.NoError(err, "PortHalfOpen() returned an error: %s", err)
+	})
+
+	t.Run("timeout 0", func(t *testing.T) {
+		test.timeout = 0
+		err := PortHalfOpen(test, server)
+		require.NoError(err, "PortHalfOpen() returned an error: %s", err)
+	})
+
+	t.Run("invalid port", func(t *testing.T) {
+		test.port = "2222"
+		err := PortHalfOpen(test, server)
+		require.Error(err, "PortHalfOpen() did not return an error")
+	})
+}
+
 func TestTCPPortOpen(t *testing.T) {
 	require := require.New(t)
 	server := testServerSetup(t)
@@ -74,6 +174,7 @@ func TestTCPPortOpen(t *testing.T) {
 		timeout:  TCPDefaultTimeout,
 	}
 
+	// TODO: Need better tests to verify proper behaviour for PortOpen.
 	t.Run("success", func(t *testing.T) {
 		require.NoError(PortOpen(test, server), "PortOpen() returned an error")
 	})
