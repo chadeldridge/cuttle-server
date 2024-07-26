5e1d9dbe28566284484eb101c69c7eb89091e3e1
Chad Eldridge <chad.eldridge@gmail.com>
2024-07-26 19:43:54 -0400

    new: Added initial AuthMiddleware for testing.

--- api/middleware.go
+++ api/middleware.go
@@ -1,14 +1,54 @@
 package api
 
-/*
-import "net/http"
+import (
+	"net/http"
 
-func newMiddleware(server HTTPServer) func(http.Handler) http.Handler {
-	// Add middleware.
-	// server.Handler = someMiddleware(server)
-	return func(h http.Handler) http.Handler {
+	"github.com/chadeldridge/cuttle/core"
+)
+
+type Middleware func(http.Handler) http.Handler
+
+// Middleware should call the next handler on success.
+// doAuth := authMiddleware(logger *core.Logger, db *db.Users) // returns func(http.Handler) http.Handler
+// mux.Handle("/v1/test", doAuth(handleTest(server.logger)))
+//
+//func newMiddleware(server HTTPServer) Middleware {
+//	// Add middleware.
+//	// server.Handler = someMiddleware(server)
+//	return func(next http.Handler) http.Handler {
+//		return http.HandlerFunc(
+//			func(w http.ResponseWriter, r *http.Request) {
+//				// Do something that fails.
+//				//if !something {
+//				//	// Return early.
+//				//	http.NotFound(w, r)
+//				//	return
+//				//}
+//
+//				// Allow original handler to run.
+//				next.ServeHTTP(w, r)
+//			})
+//	}
+//}
+
+func AuthMiddleware(logger *core.Logger) Middleware {
+	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
+				if r.Header.Get("Authorization") == "" {
+					logger.Println("no auth header")
+					err := encode(w, http.StatusUnauthorized, struct {
+						Error   string
+						Message string
+					}{
+						Error:   "unauthorized",
+						Message: "you need to login",
+					})
+					if err != nil {
+						logger.Printf("AuthMiddlware: response encode failed: %v\n", err)
+					}
+					return
+				}
 				// Do something that fails.
 				//if !something {
 				//	// Return early.
@@ -17,8 +57,7 @@ func newMiddleware(server HTTPServer) func(http.Handler) http.Handler {
 				//}
 
 				// Allow original handler to run.
-				h.ServeHTTP(w, r)
+				next.ServeHTTP(w, r)
 			})
 	}
 }
-*/
