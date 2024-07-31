4bbf2c5cfaadaea878cb90c9945cb1482d8cd21e
Chad Eldridge <chad.eldridge@gmail.com>
2024-07-31 16:13:38 -0400

    refactor: Renamed encode/decode for clarity and added html rendering and static file fetching.

--- api/routes.go
+++ api/routes.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/chadeldridge/cuttle/core"
 )
@@ -15,13 +16,14 @@ func addRoutes(mux *http.ServeMux, server *HTTPServer) error {
 	}
 
 	root.GET("/test", handleTest(server.logger), AuthMiddleware(server.logger))
+	root.GET("/login", handleLoginGet(server.logger, server))
 	// mux.Handle("/v1/test", handleTest(server.logger))
 
 	return nil
 }
 
-// func encode[T any](w http.ResponseWriter, r *http.Request, status int, obj T) error {
-func encode[T any](w http.ResponseWriter, status int, obj T) error {
+// func renderJSON[T any](w http.ResponseWriter, r *http.Request, status int, obj T) error {
+func renderJSON[T any](w http.ResponseWriter, status int, obj T) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	if err := json.NewEncoder(w).Encode(obj); err != nil {
@@ -31,8 +33,8 @@ func encode[T any](w http.ResponseWriter, status int, obj T) error {
 	return nil
 }
 
-// data, err := decode[MyStructType](r)
-func decode[T any](req *http.Request) (T, error) {
+// data, err := readJSON[MyStructType](r)
+func readJSON[T any](req *http.Request) (T, error) {
 	var obj T
 	if err := json.NewDecoder(req.Body).Decode(&obj); err != nil {
 		return obj, fmt.Errorf("decoder: %w", err)
@@ -41,11 +43,31 @@ func decode[T any](req *http.Request) (T, error) {
 	return obj, nil
 }
 
+func renderHTML(w http.ResponseWriter, status int, s string) error {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(status)
+	_, err := w.Write([]byte(s))
+	return err
+}
+
+func fetchStatic(path string) (string, error) {
+	if _, err := os.Stat(path); err != nil {
+		return "", err
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
 func handleTest(logger *core.Logger) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			logger.Debugf("test: %s\n", r.Method)
-			err := encode(w, http.StatusOK, struct{ Message string }{Message: "you did it"})
+			err := renderJSON(w, http.StatusOK, struct{ Message string }{Message: "you did it"})
 			if err != nil {
 				logger.Printf("test: %v\n", err)
 			}
