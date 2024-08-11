caaff98955ab44cae8b8634e61cc4fb082262fd6
Chad Eldridge <chad.eldridge@gmail.com>
2024-08-11 00:00:34 -0400

    refactor: Moved users table functions into sqlite.go and added tests for all current fuctions.

--- db/sqlite.go
+++ db/sqlite.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"log"
+	"time"
 
 	// libray has to be imported to register the driver.
+	"github.com/chadeldridge/cuttle/core"
 	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	// Auth Tables.
+	sqlite_tb_users       = "users"
+	sqlite_tb_user_groups = "user_groups"
 )
 
 type SqliteDB struct {
 	Name string // DB file name.
-	DB   *sql.DB
+	*sql.DB
 
 	ctx    context.Context
 	cancel func()
@@ -53,6 +60,55 @@ func (db *SqliteDB) Open() error {
 	return nil
 }
 
+func (db *SqliteDB) CuttleMigrate() error {
+	return nil
+}
+
+func (db *SqliteDB) AuthMigrate() error {
+	if err := db.UsersMigrate(); err != nil {
+		return fmt.Errorf("db.AuthMigrate: failed to migrate %s: %w", sqlite_tb_users, err)
+	}
+
+	return nil
+}
+
+// IsUnique returns nil if no records exist in the table that match the where clause. If a record
+// exists, it returns an ErrRecordExists error.
+func (db *SqliteDB) IsUnique(table string, where string, args ...any) error {
+	if table == "" {
+		return fmt.Errorf("SqliteDB.IsUnique: table is empty")
+	}
+
+	if where == "" {
+		return fmt.Errorf("SqliteDB.IsUnique: where is empty")
+	}
+
+	query := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE %s;", table, where)
+	row := db.QueryRow(query, args...)
+
+	var count int
+	err := row.Scan(&count)
+	if err != nil {
+		return fmt.Errorf("SqliteDB.IsUnique: %s", err)
+	}
+
+	if count > 0 {
+		return fmt.Errorf("SqliteDB.IsUnique: %w", ErrRecordExists)
+	}
+
+	return nil
+}
+
+func (db *SqliteDB) Close() error {
+	if db.DB == nil {
+		return nil
+	}
+
+	db.cancel()
+	return db.DB.Close()
+}
+
+/*
 // Attach the filename database tot he current database with the given alias.
 func (db *SqliteDB) Attach(filename, alias string) error {
 	if filename == "" {
@@ -129,18 +185,7 @@ func (db *SqliteDB) AddRepo(file, alias string, migrate migrater) error {
 
 	return nil
 }
-
-func (db *SqliteDB) IsUnique(query string, args ...any) bool {
-	row := db.QueryRow(query, args...)
-
-	var count int
-	err := row.Scan(&count)
-	if err != nil {
-		log.Fatalf("SqliteDB.AuthMethods.IsUnique: %s", err)
-	}
-
-	return count == 0
-}
+*/
 
 func (db *SqliteDB) Query(query string, args ...any) (*sql.Rows, error) {
 	return db.DB.QueryContext(db.ctx, query, args...)
@@ -155,11 +200,131 @@ func (db *SqliteDB) Exec(query string, args ...any) error {
 	return err
 }
 
-func (db *SqliteDB) Close() error {
-	if db.DB == nil {
-		return nil
+// ############################################################################################# //
+// ###################################        Users         #################################### //
+// ############################################################################################# //
+
+// UserData represents a user in the database.
+type UserData struct {
+	ID       int
+	Username string // Username to login with.
+	Name     string // Name to show in app.
+	Password string // Hashed password.
+	Groups   string // JSON string of group IDs. Empty should be "[]".
+	Created  time.Time
+	Updated  time.Time
+}
+
+func (db *SqliteDB) UsersMigrate() error {
+	query := `
+	CREATE TABLE IF NOT EXISTS ` + sqlite_tb_users + ` (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username VARCHAR(255) NOT NULL UNIQUE,
+		name VARCHAR(32) NOT NULL,
+		password VARCHAR(32) NOT NULL,
+		groups TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`
+
+	if err := db.Exec(query); err != nil {
+		return fmt.Errorf("db.UserMigrate: %w", err)
 	}
 
-	db.cancel()
-	return db.DB.Close()
+	return nil
+}
+
+func (db *SqliteDB) UserIsUnique(username string) error {
+	return db.IsUnique(sqlite_tb_users, "username = ?", username)
+}
+
+func (db *SqliteDB) UserCreate(username, name, pwHash, groups string) (UserData, error) {
+	if username == "" {
+		return UserData{}, fmt.Errorf("SqliteDB.UserCreate: %w", ErrInvalidUsername)
+	}
+
+	if name == "" {
+		return UserData{}, fmt.Errorf("SqliteDB.UserCreate: %w", ErrInvalidName)
+	}
+
+	// Password hash should be a 32 byte hex string.
+	if err := core.ValidatePasswordHash(pwHash); err != nil {
+		return UserData{}, fmt.Errorf("SqliteDB.UserCreate: %w", err)
+	}
+
+	if groups == "" {
+		groups = "{}"
+	}
+
+	if err := db.UserIsUnique(username); err != nil {
+		return UserData{}, fmt.Errorf("SqliteDB.UserCreate: %w", err)
+	}
+
+	query := `INSERT INTO ` + sqlite_tb_users + ` (username, name, password, groups) VALUES (?, ?, ?, ?)`
+	if err := db.Exec(query, username, name, pwHash, groups); err != nil {
+		return UserData{}, fmt.Errorf("SqliteDB.UserCreate: %w", err)
+	}
+
+	return db.UserGetByUsername(username)
+}
+
+func (db *SqliteDB) UserGet(id int) (UserData, error) {
+	query := `SELECT * FROM ` + sqlite_tb_users + ` WHERE id = ?`
+	row := db.QueryRow(query, id)
+
+	var data UserData
+	err := row.Scan(&data.ID, &data.Username, &data.Name, &data.Password, &data.Groups, &data.Created, &data.Updated)
+	if err != nil {
+		return data, fmt.Errorf("SqliteDB.UserGet: %w", err)
+	}
+
+	return data, nil
+}
+
+func (db *SqliteDB) UserGetByUsername(username string) (UserData, error) {
+	if username == "" {
+		return UserData{}, fmt.Errorf("SqliteDB.UserGetByUsername: %w", ErrInvalidUsername)
+	}
+
+	query := `SELECT * FROM ` + sqlite_tb_users + ` WHERE username = ?`
+	row := db.QueryRow(query, username)
+
+	var data UserData
+	err := row.Scan(&data.ID, &data.Username, &data.Name, &data.Password, &data.Groups, &data.Created, &data.Updated)
+	if err != nil {
+		return data, fmt.Errorf("SqliteDB.UserGetByUsername: %w", err)
+	}
+
+	return data, nil
+}
+
+func (db *SqliteDB) UserUpdate(user UserData) (UserData, error) {
+	if user.ID == 0 {
+		return UserData{}, fmt.Errorf("SqliteDB.UserUpdate: %w", ErrInvalidID)
+	}
+
+	user.Updated = time.Now()
+	query := `UPDATE ` + sqlite_tb_users + ` SET username = ?, name = ?, password = ?, groups = ?, updated_at = ? WHERE id = ?`
+	if err := db.Exec(query, user.Username, user.Name, user.Password, user.Groups, user.Updated, user.ID); err != nil {
+		return UserData{}, fmt.Errorf("SqliteDB.UserUpdate: %w", err)
+	}
+
+	return db.UserGet(user.ID)
+}
+
+func (db *SqliteDB) UserDelete(id int) error {
+	if id == 0 {
+		return fmt.Errorf("db.UserUpdate: %w", ErrInvalidID)
+	}
+
+	if _, err := db.UserGet(id); err != nil {
+		return fmt.Errorf("db.UserDelete: %w", err)
+	}
+
+	query := `DELETE FROM ` + sqlite_tb_users + ` WHERE id = ?`
+	if err := db.Exec(query, id); err != nil {
+		return fmt.Errorf("db.UserDelete: %w", err)
+	}
+
+	return nil
 }
--- db/sqlite_test.go
+++ db/sqlite_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -10,15 +11,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	testCuttleDBName = "test_cuttle.db"
+	testAuthDBName   = "test_auth.db"
+)
+
+/*
 func testDBSetup(t *testing.T) DB {
 	return testSqliteDBSetup(t)
 }
+*/
 
-func testSqliteDBSetup(t *testing.T) *SqliteDB {
+func testSqliteCuttleDBSetup(t *testing.T) *SqliteDB {
 	require := require.New(t)
-	db_folder = testDBRoot
+	SetDBRoot(testDBRoot)
 
-	db, err := NewSqliteDB("cuttle.db")
+	db, err := NewSqliteDB(testCuttleDBName)
 	require.NoError(err, "NewSqliteDB returned an error: %s", err)
 
 	err = db.Open()
@@ -26,20 +34,46 @@ func testSqliteDBSetup(t *testing.T) *SqliteDB {
 	return db
 }
 
-func deleteDBDir() {
-	err := os.RemoveAll(db_folder)
-	if err != nil {
-		log.Fatalf("deleteDBDir: %s", err)
-	}
+func testSqliteAuthDBSetup(t *testing.T) *SqliteDB {
+	require := require.New(t)
+	SetDBRoot(testDBRoot)
+
+	db, err := NewSqliteDB(testAuthDBName)
+	require.NoError(err, "NewSqliteDB returned an error: %s", err)
+
+	err = db.Open()
+	require.NoError(err, "testDBSetup returned an error: %s", err)
+	return db
 }
 
-func deleteDB(filename string) {
-	if _, err := os.Stat(db_folder + "/" + filename); os.IsNotExist(err) {
+/*
+func deleteDBDir() {
+	if db_folder == "" || db_folder == "/" {
+		log.Printf("deleteDBDir: db_folder is dangerous: %s", db_folder)
 		return
 	}
 
+	err := os.RemoveAll(db_folder)
+	if err != nil && !os.IsNotExist(err) {
+		log.Fatalf("deleteDBDir: %s", err)
+	}
+}
+*/
+
+func deleteDB(filename string) {
+	if filename == "" || filename == "/" {
+		log.Printf("deleteDBDir: db_folder is dangerous: %s", db_folder)
+		return
+	}
+
+	deleteFile(filename)
+	deleteFile(filename + "-shm")
+	deleteFile(filename + "-wal")
+}
+
+func deleteFile(filename string) {
 	err := os.Remove(db_folder + "/" + filename)
-	if err != nil {
+	if err != nil && !os.IsNotExist(err) {
 		log.Println(err)
 		log.Fatalf("deleteDB: %s", err)
 	}
@@ -49,10 +83,10 @@ func TestSqliteNewSqliteDB(t *testing.T) {
 	require := require.New(t)
 
 	t.Run("valid", func(t *testing.T) {
-		db, err := NewSqliteDB("cuttle.db")
+		db, err := NewSqliteDB(testCuttleDBName)
 		require.NoError(err, "NewSqliteDB returned an error: %s", err)
 		require.NotNil(db)
-		require.Equal("cuttle.db", db.Name)
+		require.Equal(testCuttleDBName, db.Name)
 		require.NotNil(db.ctx)
 		require.NotNil(db.cancel)
 	})
@@ -65,9 +99,58 @@ func TestSqliteNewSqliteDB(t *testing.T) {
 	})
 }
 
+func testDBMigrater(db *SqliteDB) error {
+	query := `
+	CREATE TABLE IF NOT EXISTS test (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name VARCHAR(32) NOT NULL
+	)`
+
+	if err := db.Exec(query); err != nil {
+		return fmt.Errorf("testDBMigrater: %w", err)
+	}
+
+	return nil
+}
+
+/*
+func testBadMigrater(db *SqliteDB) error {
+	if db == nil {
+		return fmt.Errorf("testBadMigrate: db is nil")
+	}
+
+	return fmt.Errorf("testBadMigrater: failed to migrate")
+}
+
+func testGetAll(db *SqliteDB) {
+	rows, err := db.Query("SELECT * FROM test")
+	if err != nil {
+		log.Fatalf("Query: %s", err)
+	}
+
+	found := false
+	for rows.Next() {
+		var id int
+		var name string
+
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatalf("Scan: %s", err)
+		}
+
+		found = true
+		log.Printf("id: %d, name: %s", id, name)
+	}
+
+	if !found {
+		log.Println("    ----    No rows found    ----")
+	}
+}
+*/
+
 func TestSqliteOpen(t *testing.T) {
 	require := require.New(t)
-	db_folder = testDBRoot
+	SetDBRoot(testDBRoot)
 	var db *SqliteDB
 
 	t.Run("empty db.Name", func(t *testing.T) {
@@ -94,16 +177,334 @@ func TestSqliteOpen(t *testing.T) {
 	})
 
 	t.Run("valid", func(t *testing.T) {
-		db, err := NewSqliteDB("cuttle.db")
+		db, err := NewSqliteDB(testCuttleDBName)
 		require.NoError(err, "NewSqliteDB returned an error: %s", err)
 
+		fmt.Printf("db: %s\n", db_folder+"/"+db.Name)
 		err = db.Open()
 		require.NoError(err, "Open returned an error: %s", err)
 		require.FileExists(db_folder+"/"+db.Name, "Open did not create the database file")
-		db.Close()
+		db.DB.Close()
+		deleteDB(testCuttleDBName)
 	})
 }
 
+func TestSqliteClose(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteCuttleDBSetup(t)
+	defer deleteDB(testCuttleDBName)
+
+	t.Run("valid", func(t *testing.T) {
+		err := db.Close()
+		require.NoError(err, "Close returned an error: %s", err)
+	})
+
+	t.Run("nil db.DB", func(t *testing.T) {
+		db.DB = nil
+		err := db.Close()
+		require.NoError(err, "Close returned an error: %s", err)
+	})
+}
+
+func TestSqliteIsUnique(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteCuttleDBSetup(t)
+	// Setup the test table.
+	testDBMigrater(db)
+	defer db.Close()
+	defer deleteDB(testCuttleDBName)
+
+	t.Run("unique", func(t *testing.T) {
+		err := db.IsUnique("test", "name = ?", "testRecord_1")
+		require.NoError(err, "IsUnique returned an error: %s", err)
+	})
+
+	// Insert a row.
+	err := db.Exec("INSERT INTO test (name) VALUES ('testRecord_1')")
+	require.NoError(err, "Exec returned an error: %s", err)
+
+	t.Run("not unique", func(t *testing.T) {
+		err := db.IsUnique("test", "name = ?", "testRecord_1")
+		require.Error(err, "IsUnique did not return an error")
+		require.ErrorIs(err, ErrRecordExists, "IsUnique did not return the expected error")
+	})
+}
+
+// ############################################################################################# //
+// ###################################        Users         #################################### //
+// ############################################################################################# //
+
+var (
+	testUser1 = UserData{
+		Username: "user1",
+		Name:     "Bob",
+		Password: "102650912390a29378e092378b29834f",
+		Groups:   "{}",
+	}
+
+	testUser2 = UserData{
+		Username: "user2",
+		Name:     "Jan",
+		Password: "102650912390a29378e092378b29834f",
+		Groups:   `[1, 45]`,
+	}
+)
+
+func TestSqliteUserCreate(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteAuthDBSetup(t)
+	defer db.Close()
+	defer deleteDB(testAuthDBName)
+
+	// Setup the test tables.
+	err := db.AuthMigrate()
+	require.NoError(err, "AuthMigrate returned an error: %s", err)
+
+	t.Run("empty username", func(t *testing.T) {
+		_, err := db.UserCreate("", testUser1.Name, testUser1.Password, testUser1.Groups)
+		require.Error(err, "UserCreate did not return an error")
+		require.ErrorIs(err, ErrInvalidUsername, "UserCreate returned an unexpected error")
+	})
+
+	t.Run("empty name", func(t *testing.T) {
+		_, err := db.UserCreate(testUser1.Username, "", testUser1.Password, testUser1.Groups)
+		require.Error(err, "UserCreate did not return an error")
+		require.ErrorIs(err, ErrInvalidName, "UserCreate returned an unexpected error")
+	})
+
+	t.Run("empty pwHash", func(t *testing.T) {
+		_, err := db.UserCreate(testUser1.Username, testUser1.Name, "", testUser1.Groups)
+		require.Error(err, "UserCreate did not return an error")
+		require.Equal(
+			"SqliteDB.UserCreate: core.ValidatePasswordHash: hash was empty",
+			err.Error(),
+			"UserCreate returned an unexpected error",
+		)
+	})
+
+	t.Run("short pwHash", func(t *testing.T) {
+		_, err := db.UserCreate(testUser1.Username, testUser1.Name, "102650912390a29", testUser1.Groups)
+		require.Error(err, "UserCreate did not return an error")
+		require.Equal(
+			"SqliteDB.UserCreate: core.ValidatePasswordHash: incorrect hash length: 15",
+			err.Error(),
+			"UserCreate returned an unexpected error",
+		)
+	})
+
+	t.Run("non-hex pwHash", func(t *testing.T) {
+		_, err := db.UserCreate(
+			testUser1.Username,
+			testUser1.Name,
+			"102650912390a29378-092378b29834f",
+			testUser1.Groups,
+		)
+		require.Error(err, "UserCreate did not return an error")
+		require.Equal(
+			"SqliteDB.UserCreate: core.ValidatePasswordHash: hash is not a hex string",
+			err.Error(),
+			"UserCreate returned an unexpected error",
+		)
+	})
+
+	t.Run("empty groups", func(t *testing.T) {
+		_, err := db.UserCreate(testUser1.Username, testUser1.Name, testUser1.Password, "")
+		require.NoError(err, "UserCreate returned an error: %s", err)
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		got, err := db.UserCreate(testUser2.Username, testUser2.Name, testUser2.Password, testUser2.Groups)
+		require.NoError(err, "UserCreate returned an error: %s", err)
+		require.Equal(testUser2.Username, got.Username)
+		require.Equal(testUser2.Name, got.Name)
+		require.Equal(testUser2.Password, got.Password)
+		require.Equal(testUser2.Groups, got.Groups)
+		require.NotZero(got.Created)
+		require.NotZero(got.Updated)
+	})
+}
+
+func TestSqliteUserIsUnique(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteAuthDBSetup(t)
+	defer db.Close()
+	defer deleteDB(testAuthDBName)
+
+	// Setup the test tables.
+	err := db.AuthMigrate()
+	require.NoError(err, "AuthMigrate returned an error: %s", err)
+
+	t.Run("unique", func(t *testing.T) {
+		err := db.UserIsUnique(testUser1.Username)
+		require.NoError(err, "IsUnique returned an error: %s", err)
+	})
+
+	// Insert a row.
+	_, err = db.UserCreate(testUser1.Username, testUser1.Name, testUser1.Password, testUser1.Groups)
+	require.NoError(err, "Exec returned an error: %s", err)
+
+	t.Run("not unique", func(t *testing.T) {
+		err := db.UserIsUnique(testUser1.Username)
+		require.Error(err, "IsUnique did not return an error")
+		require.ErrorIs(err, ErrRecordExists, "IsUnique did not return the expected error")
+	})
+}
+
+func TestSqliteUserGet(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteAuthDBSetup(t)
+	defer db.Close()
+	defer deleteDB(testAuthDBName)
+
+	// Setup the test tables.
+	err := db.AuthMigrate()
+	require.NoError(err, "AuthMigrate returned an error: %s", err)
+
+	// Insert a row.
+	want, err := db.UserCreate(testUser1.Username, testUser1.Name, testUser1.Password, testUser1.Groups)
+	require.NoError(err, "Exec returned an error: %s", err)
+
+	t.Run("valid", func(t *testing.T) {
+		data, err := db.UserGet(1)
+		require.NoError(err, "UserGet returned an error: %s", err)
+		require.Equal(want.Username, data.Username)
+		require.Equal(want.Name, data.Name)
+		require.Equal(want.Password, data.Password)
+		require.Equal(want.Groups, data.Groups)
+		require.Equal(want.Created, data.Created)
+		require.Equal(want.Updated, data.Updated)
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		data, err := db.UserGet(9999)
+		require.Error(err, "UserGet did not return an error")
+		require.ErrorIs(err, sql.ErrNoRows, "UserGet did not return the expected error")
+		require.Equal(UserData{}, data)
+	})
+}
+
+func TestSqliteUserGetByUsername(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteAuthDBSetup(t)
+	defer db.Close()
+	defer deleteDB(testAuthDBName)
+
+	// Setup the test tables.
+	err := db.AuthMigrate()
+	require.NoError(err, "AuthMigrate returned an error: %s", err)
+
+	// Insert a row.
+	_, err = db.UserCreate(testUser1.Username, testUser1.Name, testUser1.Password, testUser1.Groups)
+	require.NoError(err, "Exec returned an error: %s", err)
+
+	t.Run("empty username", func(t *testing.T) {
+		data, err := db.UserGetByUsername("")
+		require.Error(err, "UserGetByUsername did not return an error")
+		require.ErrorIs(err, ErrInvalidUsername, "UserGetByUsername did not return the expected error")
+		require.Equal(UserData{}, data)
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		data, err := db.UserGetByUsername("not_a_user")
+		require.Error(err, "UserGetByUsername did not return an error")
+		require.ErrorIs(err, sql.ErrNoRows, "UserGetByUsername did not return the expected error")
+		require.Equal(UserData{}, data)
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		data, err := db.UserGetByUsername(testUser1.Username)
+		require.NoError(err, "UserGet returned an error: %s", err)
+		require.Equal(testUser1.Username, data.Username)
+		require.Equal(testUser1.Name, data.Name)
+		require.Equal(testUser1.Password, data.Password)
+		require.Equal(testUser1.Groups, data.Groups)
+	})
+}
+
+func TestSqliteUserUpdate(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteAuthDBSetup(t)
+	defer db.Close()
+	defer deleteDB(testAuthDBName)
+
+	// Setup the test tables.
+	err := db.AuthMigrate()
+	require.NoError(err, "AuthMigrate returned an error: %s", err)
+
+	// Insert a row.
+	data, err := db.UserCreate(testUser1.Username, testUser1.Name, testUser1.Password, testUser1.Groups)
+	require.NoError(err, "Exec returned an error: %s", err)
+
+	t.Run("invalid ID", func(t *testing.T) {
+		_, err := db.UserUpdate(UserData{ID: 0})
+		require.Error(err, "UserUpdate did not return an error")
+		require.ErrorIs(err, ErrInvalidID, "UserUpdate did not return the expected error")
+	})
+
+	createdAt := data.Created
+	updatedAt := data.Updated
+
+	t.Run("valid", func(t *testing.T) {
+		data.Username = testUser2.Username
+		data.Name = testUser2.Name
+		data.Password = testUser2.Password
+		data.Groups = testUser2.Groups
+
+		updated, err := db.UserUpdate(data)
+		require.NoError(err, "UserUpdate returned an error: %s", err)
+		require.Equal(data.Username, updated.Username)
+		require.Equal(data.Name, updated.Name)
+		require.Equal(data.Password, updated.Password)
+		require.Equal(data.Groups, updated.Groups)
+		require.Equal(createdAt, updated.Created)
+		require.Greater(updated.Updated, updatedAt)
+	})
+
+	t.Run("invalid id", func(t *testing.T) {
+		data.ID = 9999
+		_, err := db.UserUpdate(data)
+		require.Error(err, "UserUpdate did not return an error")
+		require.ErrorIs(err, sql.ErrNoRows, "UserUpdate did not return the expected error")
+	})
+}
+
+func TestSqliteUserDelete(t *testing.T) {
+	require := require.New(t)
+	db := testSqliteAuthDBSetup(t)
+	defer db.Close()
+	defer deleteDB(testAuthDBName)
+
+	// Setup the test tables.
+	err := db.AuthMigrate()
+	require.NoError(err, "AuthMigrate returned an error: %s", err)
+
+	// Insert a row.
+	data, err := db.UserCreate(testUser1.Username, testUser1.Name, testUser1.Password, testUser1.Groups)
+	require.NoError(err, "Exec returned an error: %s", err)
+
+	t.Run("invalid ID", func(t *testing.T) {
+		err := db.UserDelete(0)
+		require.Error(err, "UserDelete did not return an error")
+		require.ErrorIs(err, ErrInvalidID, "UserDelete did not return the expected error")
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		err := db.UserDelete(data.ID)
+		require.NoError(err, "UserDelete returned an error: %s", err)
+
+		_, err = db.UserGet(data.ID)
+		require.Error(err, "UserGet did not return an error")
+		require.ErrorIs(err, sql.ErrNoRows, "UserGet did not return the expected error")
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		err := db.UserDelete(9999)
+		require.Error(err, "UserDelete did not return an error")
+		require.ErrorIs(err, sql.ErrNoRows, "UserDelete did not return the expected error")
+	})
+}
+
+/*
 func TestSqliteAttach(t *testing.T) {
 	require := require.New(t)
 	db_folder = testDBRoot
@@ -184,28 +585,6 @@ func TestSqliteIsAttached(t *testing.T) {
 	deleteDB(db_file)
 }
 
-func testDBMigrater(db DB) error {
-	query := `
-	CREATE TABLE IF NOT EXISTS test (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name VARCHAR(255) NOT NULL
-	)`
-
-	if err := db.Exec(query); err != nil {
-		return fmt.Errorf("testDBMigrater: %w", err)
-	}
-
-	return nil
-}
-
-func testBadMigrater(db DB) error {
-	if db == nil {
-		return fmt.Errorf("testBadMigrate: db is nil")
-	}
-
-	return fmt.Errorf("testBadMigrater: failed to migrate")
-}
-
 func TestSqliteAddRepo(t *testing.T) {
 	require := require.New(t)
 	db_folder = testDBRoot
@@ -257,3 +636,4 @@ func TestSqliteAddRepo(t *testing.T) {
 
 	deleteDB(db_file)
 }
+*/
