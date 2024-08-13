a4c9c362253ac41185113b2d972c545f04dd40ec
Chad Eldridge <chad.eldridge@gmail.com>
2024-08-13 00:46:33 -0400

    refactor: Updated AuthDB interface to match current available methods.

--- db/db.go
+++ db/db.go
@@ -41,10 +41,18 @@ type AuthDB interface {
 	// Users
 	UserIsUnique(username string) error
 	UserCreate(username, name, password, groups string) (UserData, error)
-	UserGet(id int) (UserData, error)
+	UserGet(id int64) (UserData, error)
 	UserGetByUsername(username string) (UserData, error)
 	UserUpdate(user UserData) (UserData, error)
-	UserDelete(id int) error
+	UserDelete(id int64) error
+	// UserGroups
+	UserGroupIsUnique(name string) error
+	UserGroupCreate(name, members, profiles string) (UserGroupData, error)
+	UserGroupGet(id int64) (UserGroupData, error)
+	UserGroupGetByName(name string) (UserGroupData, error)
+	UserGroupGetGroups(ids []int64) ([]UserGroupData, error)
+	UserGroupUpdate(group UserGroupData) (UserGroupData, error)
+	UserGroupDelete(id int64) error
 }
 
 // SetDBRoot sets the root directory for the database. If this is not set, the default behavior is to
